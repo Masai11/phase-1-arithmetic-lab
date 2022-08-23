5b32e41f212548c650dcaea3e437d36c28173678
Masai11 <roy.kiplagat@student.moringaschool.com>
2022-08-23 07:40:48 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+console.log(multiply)
+
+const random = Math.floor(Math.random() * 10) + 1;
+console.log(random)
+
+let num3 = 14
+let num4 = 3
+let mod = (num3 % num4)+2
+console.log(mod)
+
+const max = Math.max(0,5,10,15,20)
+console.log(max)
